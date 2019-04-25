e88c79dd107babbfd1985d9401fe70aa3f07b3e6
ydl <yangdeliang@juwang.cn>
2019-04-25 11:25:20 +0800

    fix: miss git log message

--- send-email-with-git.py
+++ send-email-with-git.py
@@ -10,8 +10,10 @@ from email.mime.text import MIMEText
 from config import *
 
 
-def get_git_log(pro_path, author):
-    command = "cd %s && git log --author=%s | grep -P '(feat|fix:style)' | head && cd -" % (pro_path, author)
+def get_git_log(pro_path, author, env):
+    types = 'feat|fix|docs|style|refactor|perf|test|chore|revert'
+    command = "cd %s && git checkout %s && git pull && git log --author=%s | grep -P '(%s)' | head && cd -"\
+              % (pro_path, env, author, types)
     result = os.popen(command).readlines()
     return list(map(lambda x: x.strip(), result))
 
@@ -33,11 +35,21 @@ def send_mail(sf, st, msg):
 
 
 def parse_config():
-    log = ["  "] + get_git_log('/mnt/api-site', 'ydl')
     cli = VerticalPrompt(
         [
             Bullet("choice env? ",
                    choices=['test', 'new-test']),
+        ],
+        spacing=1
+    )
+    result = cli.launch()
+    _, subj = result[0]
+    if subj == 'new-test':
+        subj = 'test-next'
+
+    log = ["  "] + get_git_log('/mnt/api-site', 'ydl', subj)
+    cli = VerticalPrompt(
+        [
             Bullet("choice git log: ",
                    choices=log),
             YesNo("send? "),
@@ -45,9 +57,8 @@ def parse_config():
         spacing=1
     )
     result = cli.launch()
-    _, subj = result[0]
-    _, text = result[1]
-    _, yes_or_no = result[2]
+    _, text = result[0]
+    _, yes_or_no = result[1]
     return subj, text, yes_or_no
 
 
@@ -55,8 +66,9 @@ if __name__ == '__main__':
     _subject, _content, yes = parse_config()
     if len(sys.argv) >= 2:
         _content += "\n" + sys.argv[1]
+    _subject = 'party %s 有更新' % _subject
     _msg = parse_message(_subject, _content)
-    print((_content, _msg.as_string()))
+    print((_subject, _content, _msg.as_string()))
     if yes and send_mail(send_from, send_to, _msg):
         print('success')
     else:
